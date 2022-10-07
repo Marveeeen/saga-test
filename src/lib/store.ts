ed4892ce69f9e36fd6ced0ea6b37de3199c71716
Marveeen <jmvroqueee@gmail.com>
2022-10-07 21:39:03 +0800

    Add CRUD saga and action

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,6 +1,6 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "@redux-saga/core";
-import { Todo, getTodos } from "./api";
+import { Todo, getTodos, updateTodo, deleteTodo, createTodo } from "./api";
 import { put, takeEvery } from "redux-saga/effects";
 
 function* getTodosAction() {
@@ -8,8 +8,42 @@ function* getTodosAction() {
   yield put({ type: "TODOS_FETCH_SUCCEEDED", payload: todos });
 }
 
+function* createTodoAction({
+  payload,
+} : {
+  type: "CREATE_TODO_REQUESTED"
+  payload: string
+}) {
+  yield createTodo(payload)
+  yield put({ type: "TODOS_FETCH_REQUESTED" })
+}
+
+function* updateTodoAction({
+  payload,
+} : {
+  type: "UPDATE_TODO_REQUESTED"
+  payload: Todo
+}) {
+  yield updateTodo(payload)
+  yield put({ type: "TODOS_FETCH_REQUESTED" })
+}
+
+function* deleteTodoAction({
+  payload,
+} : {
+  type: "DELETE_TODO_REQUESTED"
+  payload: Todo
+}) {
+  yield deleteTodo(payload)
+  yield put({ type: "TODOS_FETCH_REQUESTED" })
+}
+
 function* rootSaga() {
   yield takeEvery("TODOS_FETCH_REQUESTED", getTodosAction);
+  yield takeEvery("CREATE_TODO_REQUESTED", createTodoAction);
+  yield takeEvery("UPDATE_TODO_REQUESTED", updateTodoAction);
+  yield takeEvery("DELETE_TODO_REQUESTED", deleteTodoAction);
+
 }
 
 const reducer = (
@@ -28,3 +62,27 @@ const sagaMiddleWare = createSagaMiddleware();
 export const store = createStore(reducer, applyMiddleware(sagaMiddleWare));
 
 sagaMiddleWare.run(rootSaga);
+
+export const selectTodos = (state: Todo[]) => state;
+
+export const fetchTodos = () => ({
+  type: 'TODOS_FETCH_REQUESTED'
+})
+
+export const toggleTodo = (todo: Todo) => ({
+  type: 'UPDATE_TODO_REQUESTED',
+  payload: {
+    ...todo,
+    done: !todo.done
+  }
+})
+
+export const removeTodo = (todo: Todo) => ({
+  type: "DELETE_TODO_REQUESTED",
+  payload: todo,
+})
+
+export const addTodo = (text: string) => ({
+  type: "CREATE_TODO_REQUESTED",
+  payload: text
+})
